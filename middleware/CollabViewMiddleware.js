c423d1239c2d5097994dc82b4eeb3e4ca32e8be2
Atheesh Thirumalairajan <github-commits@atheesh.org>
2022-10-25 10:22:33 +0530

    Added Multiple Features

--- middleware/CollabViewMiddleware.js
+++ middleware/CollabViewMiddleware.js
@@ -6,11 +6,52 @@ const CollabViewMiddleware = (httpServer) => {
         }
     });
 
+    const volatileActiveUsers = {};
     io.on('connection', (socket) => {
-        console.log(`Client Connected: ${socket.id}`);
+        let joinedRoom = null;
+
+        socket.on('cbv-joinRoom', (e) => {
+            socket.join(e.roomName);
+            joinedRoom = e.roomName;
+
+            volatileActiveUsers[e.roomName] = [];
+            volatileActiveUsers[e.roomName].push({
+
+            })
+        })
+
+        socket.on('cbv-newActiveUser', (e) => {
+            socket.join(e.roomName);
+            joinedRoom = e.roomName;
+
+            if (volatileActiveUsers[e.roomName] == undefined)
+                volatileActiveUsers[e.roomName] = [];
+
+            volatileActiveUsers[e.roomName].push({
+                uName: e.uName,
+                uId: e.uId,
+                roomName: e.roomName
+            })
+
+            socket.broadcast.emit('cbv-newActiveUser', e);
+            io.to(socket.id).emit('cbv-cachedActiveUsersList', volatileActiveUsers[e.roomName]);
+        });
+
         socket.on('cbv-nibPosition', (e) => {
             socket.broadcast.emit('cbv-nibPosition', e);
-        })
+        });
+
+        socket.on('disconnect', (e) => {
+            console.log(`${socket.id} disconnected\n`)
+
+            volatileActiveUsers[joinedRoom]
+                = volatileActiveUsers[joinedRoom]
+                    .filter((activeUser) => (activeUser.uId != socket.id))
+
+            io.in(joinedRoom).emit('cbv-delActiveUser', {
+                uId: socket.id
+            });
+        });
     })
 }
 
