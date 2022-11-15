58f18f24bbb8d8ac8d17e6a5720702d7319833b9
Atheesh Thirumalairajan <github-commits@atheesh.org>
2022-11-15 18:05:27 +0530

    Huge changes in framework, just progressing

--- middleware/CollabViewMiddleware.js
+++ middleware/CollabViewMiddleware.js
@@ -9,39 +9,27 @@ const CollabViewMiddleware = (httpServer) => {
         }
     });
 
-    let mediasoupWorker = null;
+    let vcWorker = undefined;
     const volatileRooms = {};
     const volatileWbStates = {};
 
-    const initializeMediasoupWorker = () => {
-        const mediasoupWorkerPromise = new Promise(async (resolve, reject) => {
-            let mediasoupWorker = await mediasoup.createWorker({
-                rtcMinPort: 2000,
-                rtcMaxPort: 2020
-            });
-
-            if (mediasoupWorker) resolve(mediasoupWorker);
-        });
-
-        mediasoupWorkerPromise
-            .then((worker) => {
-                // Log Worker Information
-                mediasoupWorker = worker;
-                console.log(`Mediasoup Worker PID: ${worker.pid}`);
-
-                // Worker Event 'Error'
-                worker.on('died', error => {
-                    console.error("Mediasoup Worker Died. Attempting Restart in 5 seconds...");
-                    setTimeout(() => {
-                        initializeMediasoupWorker();
-                    }, 5000);
+    const initializeMediasoupWorker = (callback) => {
+        callback(
+            new Promise(async (resolve, reject) => {
+                let mediasoupWorker = await mediasoup.createWorker({
+                    rtcMinPort: 2000,
+                    rtcMaxPort: 2020
                 });
-            });
+
+                if (mediasoupWorker) resolve(mediasoupWorker);
+                else reject("Failed to create Mediasoup Worker...");
+            })
+        );
     }
 
     const initializeMediaRouter = (callback) => {
         const mediasoupRouterPromise = new Promise(async (resolve, reject) => {
-            if (mediasoupWorker == null) reject("Mediasoup Worker is Null.");
+            if (vcWorker == null) reject("Mediasoup Worker is Null.");
             const mediaCodecs = [
                 {
                     kind: "audio",
@@ -63,7 +51,7 @@ const CollabViewMiddleware = (httpServer) => {
             ];
 
             resolve(
-                await mediasoupWorker.createRouter({ mediaCodecs })
+                await vcWorker.createRouter({ mediaCodecs })
             );
         });
 
@@ -72,7 +60,7 @@ const CollabViewMiddleware = (httpServer) => {
             .catch(() => {
                 console.error("Router Creation Failed. Retrying in 5 seconds");
                 setTimeout(() => {
-                    initializeMediaRouter (callback);
+                    initializeMediaRouter(callback);
                 }, 5000);
             })
     }
@@ -86,93 +74,127 @@ const CollabViewMiddleware = (httpServer) => {
     }
 
     // Init. SFU Server Worker
-    initializeMediasoupWorker();
+    initializeMediasoupWorker((mediasoupWorkerPromise) => {
+        mediasoupWorkerPromise
+            .then((worker) => {
+                // Log Worker Information
+                vcWorker = worker;
+                console.log(`VC_WORKER_CREATE: PROCESS_ID ${worker.pid}`);
+
+                // Worker Event 'Error'
+                worker.on('died', error => {
+                    console.error("Mediasoup Worker Died. Attempting Restart in 5 seconds...");
+                    setTimeout(() => {
+                        initializeMediasoupWorker();
+                    }, 5000);
+                });
+            })
+
+            .catch(() => {
+                console.error("Failed to create Mediasoup Worker, Attempting creation in 5 seconds...");
+                setTimeout(() => {
+                    initializeMediasoupWorker();
+                }, 5000);
+            })
+    });
 
     io.on('connection', (socket) => {
+        // Per user room status (used to update exits)
         let joinedRoom = null;
 
-        socket.on('cbv-newActiveUser', (e) => {
-            socket.join(e.roomName);
-            joinedRoom = e.roomName;
+        const initializeNewFlowwRoom = (roomName) => {
+            volatileRooms[roomName] = {};
+            volatileWbStates[roomName] = {};
+            volatileRooms[roomName].activeUsers = {};
 
-            if (volatileRooms[e.roomName] == undefined)
-                volatileRooms[e.roomName] = {};
-
-            if (volatileRooms[e.roomName].vc_router == undefined) {
-                initializeMediaRouter((router) => {
-                    volatileRooms[e.roomName].vc_router = router;
-                });
-            }
-
-            if (volatileWbStates[e.roomName] == undefined)
-                volatileWbStates[e.roomName] = {};
-
-            if (volatileRooms[e.roomName].activeUsers == undefined)
-                volatileRooms[e.roomName].activeUsers = {};
-
-            let parsedActiveUsers = [];
-            Object.keys(volatileRooms[e.roomName].activeUsers).forEach((activeUser) => {
-                parsedActiveUsers.push(volatileRooms[e.roomName].activeUsers[activeUser]);
+            initializeMediaRouter((router) => {
+                console.log(`VC_ROUTER_ASSIGN: RoomID ${roomName} -> RouterID ${router.id}`);
+                volatileRooms[roomName].vcRouter = router;
             });
+        }
 
-            io.to(socket.id).emit('cbv-cachedActiveUsersList', parsedActiveUsers);
-            volatileRooms[e.roomName].activeUsers[e.uId] = {
-                uName: e.uName,
-                uId: e.uId,
-                vcTransport: null
-            }
-
-            //Send Required States to new user
-            io.to(socket.id).emit('cbv-volatileStates', volatileWbStates[e.roomName]);
-
-            //Notify users in room about new user
-            socket.to(e.roomName).emit('cbv-newActiveUser', e);
-        });
-
-        socket.on("cbv-vcRtpCapabilities", (e) => {
-            io.to(socket.id).emit('cbv-vcRtpCapabilities', {
-                rtpCapabilities: volatileRoomRouters[roomName].rtpCapabilities
+        const initializePreJoinListeners = () => {
+            socket.on("cbv-createRoom", (e) => {
+                
             })
-        });
+        }
 
-        socket.on("cbv-vcCreateWebRTCTransport", (e) => {
-            if (e.producer == true) {
-
-            }
-        });
-
-        socket.on('cbv-createSelection', (e) => {
-            socket.to(e.roomName).emit('cbv-createSelection', e);
-        });
-
-        socket.on('cbv-nibPosition', (e) => {
-            socket.to(e.roomName).emit('cbv-nibPosition', e);
-        });
-
-        socket.on('cbv-nibPress', (e) => {
-            volatileWbStates[e.roomName].wbOverlayDisableRef = true;
-            socket.to(e.roomName).emit('cbv-nibPress', e);
-        });
-
-        socket.on('cbv-nibLift', (e) => {
-            volatileWbStates[e.roomName].wbOverlayDisableRef = false;
-            socket.to(e.roomName).emit('cbv-nibLift', e);
-        });
-
-        socket.on('cbv-comment', (e) => {
-            e.time = moment().toISOString();
-            socket.to(e.roomName).emit('cbv-comment', e);
-        })
-
-        socket.on('disconnect', (e) => {
-            // Log disconnection
-            console.log(`${socket.id} disconnected\n`)
-
-            delete volatileRooms[joinedRoom].activeUsers[socket.id]
-            io.in(joinedRoom).emit('cbv-delActiveUser', {
-                uId: socket.id
+        const initializeConferenceListeners = () => {
+            socket.on("cbv-vcRouterRtpCapabilities", (e) => {
+                e.callback(volatileRooms[e.roomName].vcRouter.rtpCapabilities);
             });
-        });
+    
+            socket.on("cbv-vcProduceMedia", (e) => {
+    
+            });
+        }
+
+        const initializeActiveUsersListeners = () => {
+            socket.on('cbv-newActiveUser', (e) => {
+                socket.join(e.roomName);
+                joinedRoom = e.roomName;
+    
+                if (volatileRooms[e.roomName] == undefined)
+                    initializeNewFlowwRoom(e.roomName);
+    
+                let parsedActiveUsers = [];
+                Object.keys(volatileRooms[e.roomName].activeUsers).forEach((activeUser) => {
+                    parsedActiveUsers.push(volatileRooms[e.roomName].activeUsers[activeUser]);
+                });
+    
+                io.to(socket.id).emit('cbv-cachedActiveUsersList', parsedActiveUsers);
+                volatileRooms[e.roomName].activeUsers[e.uId] = {
+                    uName: e.uName,
+                    uId: e.uId,
+                    vcTransport: null
+                }
+    
+                //Send Required States to new user
+                io.to(socket.id).emit('cbv-volatileStates', volatileWbStates[e.roomName]);
+    
+                //Notify users in room about new user
+                socket.to(e.roomName).emit('cbv-newActiveUser', e);
+            });
+
+            socket.on('disconnect', (e) => {
+                // Log disconnection
+                console.log(`${socket.id} disconnected\n`)
+    
+                if (volatileRooms[joinedRoom].activeUsers != undefined)
+                    delete volatileRooms[joinedRoom].activeUsers[socket.id]
+    
+                io.in(joinedRoom).emit('cbv-delActiveUser', {
+                    uId: socket.id
+                });
+            });
+        }
+
+        const initializeDrawingBoardListeners = () => {
+            socket.on('cbv-createSelection', (e) => {
+                socket.to(e.roomName).emit('cbv-createSelection', e);
+            });
+    
+            socket.on('cbv-nibPosition', (e) => {
+                socket.to(e.roomName).emit('cbv-nibPosition', e);
+            });
+    
+            socket.on('cbv-nibPress', (e) => {
+                volatileWbStates[e.roomName].wbOverlayDisableRef = true;
+                socket.to(e.roomName).emit('cbv-nibPress', e);
+            });
+    
+            socket.on('cbv-nibLift', (e) => {
+                volatileWbStates[e.roomName].wbOverlayDisableRef = false;
+                socket.to(e.roomName).emit('cbv-nibLift', e);
+            });
+        }
+
+        const initializeCommentsListeners = () => {
+            socket.on('cbv-comment', (e) => {
+                e.time = moment().toISOString();
+                socket.to(e.roomName).emit('cbv-comment', e);
+            })
+        }
     })
 }
 
